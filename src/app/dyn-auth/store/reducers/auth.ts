26ce31c02c44086ca4bef22011a01ec41a7d75bb
Dean Selvey <deanselvey@gmail.com>
2018-02-18 21:39:47 +0000

    Fixed issue with reducer not having default

--- src/app/dyn-auth/store/reducers/auth.ts
+++ src/app/dyn-auth/store/reducers/auth.ts
@@ -36,6 +36,9 @@ export function reducer(state = initialState, action: AuthActions) {
 
     case AuthActionTypes.LOGOUT:
       return {...state, loggedIn: false, loading: true}
+
+    default:
+      return state;
   }
 }
 
