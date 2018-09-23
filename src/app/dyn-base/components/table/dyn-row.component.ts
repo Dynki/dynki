5dc1b9ae56a2a8e205a30a5546c0646693129b1b
Dean Selvey <deanselvey@gmail.com>
2018-09-23 19:00:36 +0100

    Delete confirmation for rows

--- src/app/dyn-base/components/table/dyn-row.component.ts
+++ src/app/dyn-base/components/table/dyn-row.component.ts
@@ -7,7 +7,10 @@ import * as boardActions from '../../../dyn-boards/store/board.actions'
     template: `
     <div class="table__row">
         <dyn-cell *ngFor="let column of columns; index as i; first as isFirst" [column]="column" [row]="row" [action]="action"></dyn-cell>
-        <i class="anticon anticon-delete table__row__delete__icon" nz-tooltip nzTitle="Delete Row" (click)="deleteRow(row)"></i>
+        <i
+            class="anticon anticon-delete table__row__delete__icon"
+            nz-tooltip
+            nz-popconfirm nzTitle="Are you sure delete this row?" (nzOnConfirm)="deleteRow(row)"></i>
     </div>
     `
 })
