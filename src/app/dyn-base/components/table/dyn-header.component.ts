dd6ecea72c4072cc8311092dda814614b45484f3
Dean Selvey <deanselvey@gmail.com>
2018-10-02 20:43:00 +0100

    Column header menu feature

--- src/app/dyn-base/components/table/dyn-header.component.ts
+++ src/app/dyn-base/components/table/dyn-header.component.ts
@@ -7,19 +7,37 @@ import { Store } from '@ngxs/store';
   template: `
     <div class="table__header">
         <div class="table__header__columns">
-            <input *ngFor="let column of columns; first as isFirst"
+            <div *ngFor="let column of columns; first as isFirst" class="table__header__columns__container">
+                <div class="table__column">
+                    <nz-dropdown [nzTrigger]="'click'" class="table__column__menu" [nzPlacement]="'bottomCenter'" [nzClickHide]="false">
+                        <button nz-dropdown nz-button nzType="default" [nzSize]="'small'" nzShape="circle">
+                            <i class="anticon anticon-down"></i>
+                        </button>
+                        <ul nz-menu>
+                            <li nz-menu-item class="table__column__menu__item" (click)="removeColumn(column)">
+                                <i class="anticon anticon-delete"></i>
+                                <span> Remove Column</span>
+                            </li>
+                        </ul>
+                    </nz-dropdown>
+                </div>
+                <input
                 [(ngModel)]="column.title" (blur)="updateColumn(column)" class="table__header__input text--no-border" type="text">
+            </div>
         </div>
-        <nz-dropdown [nzTrigger]="'click'" class="table__header__menu" [nzPlacement]="'bottomRight'" [nzClickHide]="false">
-            <i nz-dropdown nz-tooltip [nzPlacement]="'left'"
-                [nzTitle]="'Add Column'" class="table__header__menu__icon anticon anticon-plus-circle"></i>
-            <ul nz-menu>
-                <li nz-menu-item class="table__header__menu__item" (click)="addColumn('text')">
-                    <i class="anticon anticon-form"></i>
-                    <span>Text</span>
-                </li>
-            </ul>
-        </nz-dropdown>
+        <div class="table__header__menu__container">
+            <nz-dropdown [nzTrigger]="'click'"
+            class="table__header__menu__container__dropdown" [nzPlacement]="'bottomRight'" [nzClickHide]="false">
+                <i nz-dropdown nz-tooltip [nzPlacement]="'left'"
+                    [nzTitle]="'Add Column'" class="table__header__menu__container__dropdown__icon anticon anticon-plus-circle"></i>
+                <ul nz-menu>
+                    <li nz-menu-item class="table__header__menu__container__dropdown__item" (click)="addColumn('text')">
+                        <i class="anticon anticon-form"></i>
+                        <span>Text</span>
+                    </li>
+                </ul>
+            </nz-dropdown>
+        </div>
     </div>
 `
 })
