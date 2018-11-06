6ee5e3d5e98f85e4a31743b918655c41464dcab6
Dean Selvey <deanselvey@gmail.com>
2018-11-06 21:06:34 +0000

    Amendments to domain choices

--- src/app/dyn-shell/components/domain-registration/dyn-domain-choice.component.ts
+++ src/app/dyn-shell/components/domain-registration/dyn-domain-choice.component.ts
@@ -4,34 +4,18 @@ import { Component } from '@angular/core';
     selector: 'dyn-domain-choice',
     template: `
     <div class="domain__choice">
+        <div class="brand">
+            <div class="section__img"></div>
+            <h1>Dynki</h1>
+        </div>
 
         <section class="section">
-            <div class="domain__container">
-                <div class="brand">
-                    <div class="section__img"></div>
-                    <h1>Dynki</h1>
-                </div>
+            <div class="main__form">
+                <div class="new">Create new domain</div>
+                <div class="or">Or</div>
+                <div class="join">Join existing domain</div>
             </div>
-            <div class="main">
-                <nz-card style="width:300px;" [nzCover]="coverTemplate" [nzActions]="[actionSetting,actionEdit,actionEllipsis]">
-                    <nz-card-meta nzTitle="Card title" nzDescription="This is the description" [nzAvatar]="avatarTemplate"></nz-card-meta>
-                </nz-card>
-                <ng-template #avatarTemplate>
-                    <nz-avatar nzSrc="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"></nz-avatar>
-                </ng-template>
-                <ng-template #coverTemplate>
-                    <img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>
-                </ng-template>
-                <ng-template #actionSetting>
-                    <i nz-icon type="setting"></i>
-                </ng-template>
-                <ng-template #actionEdit>
-                    <i nz-icon type="edit"></i>
-                </ng-template>
-                <ng-template #actionEllipsis>
-                    <i nz-icon type="ellipsis"></i>
-                </ng-template>
-            </div>
+            <div class="main__pic"></div>
         </section>
     </div>`
 })
