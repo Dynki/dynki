3770428cf64527bd32c44d0e71a03b25ecf8f5a6
Dean Selvey <deanselvey@gmail.com>
2018-11-24 10:07:04 +0000

    Styling changes to messages

--- src/app/dyn-messaging/components/dyn-message-list.component.ts
+++ src/app/dyn-messaging/components/dyn-message-list.component.ts
@@ -5,10 +5,30 @@ import { IMessages } from '../store/message.model';
     selector: 'dyn-msglist, [dyn-msglist]',
     template: `
     <section class="msglist">
-        <div *ngFor="let msg of data.messages; index as i">
-            <nz-card>
-                <p>{{msg.subject}}</p>
-                <p>{{msg.body}}</p>
+        <div class="header">
+            <nz-select class="sort" nzPlaceHolder="Sort" [(ngModel)]="sortOrder">
+                <nz-option nzValue="recent" nzLabel="Recent"></nz-option>
+                <nz-option nzValue="oldest" nzLabel="Oldest"></nz-option>
+                <nz-option nzValue="from" nzLabel="From"></nz-option>
+            </nz-select>
+            <nz-switch class="switch" nzCheckedChildren="Un-Read" nzUnCheckedChildren="All Mail"></nz-switch>
+        </div>
+        <div class="msgs" *ngFor="let msg of data.messages; index as i">
+            <nz-card class="msgs__card">
+                <div class="panel-left">
+                    <div class="avatar">D</div>
+                </div>
+                <div class="panel-right">
+                    <div class="first-line">
+                        <div class="from">{{msg.from}}</div>
+                        <div class="created">10.43 AM</div>
+                    </div>
+                    <div class="subject">
+                        <h1>{{msg.subject}}</h1>
+                        <nz-badge nzStatus="processing"></nz-badge>
+                    </div>
+                    <p class="body">{{msg.body}}</p>
+                </div>
             </nz-card>
         </div>
     </section>
@@ -18,5 +38,8 @@ export class DynMessagingListComponent {
 
     @Input() data: IMessages;
 
-    constructor() { }
+    sortOrder = 'recent';
+
+    constructor() {
+    }
 }
