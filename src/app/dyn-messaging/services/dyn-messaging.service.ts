72fa1d030e33721d0b04db4457c5e3322eb028eb
Dean Selvey <deanselvey@gmail.com>
2018-12-01 20:26:44 +0000

    Get messages from db

--- src/app/dyn-messaging/services/dyn-messaging.service.ts
+++ src/app/dyn-messaging/services/dyn-messaging.service.ts
@@ -16,69 +16,6 @@ import * as moment from 'moment';
 export class MessagingService {
 
   private userInfo: UserInfo;
-  private messages: IMessages = {
-    messages: [
-      {
-        id: '232425t34534',
-        from: 'Dynki Team',
-        to: ['Dean Selvey'],
-        subject: 'Welcome to Dynki',
-        body: {
-          ops: [
-          { insert: 'Hi @Dean Selvey, \n\n' +
-          'Thanks for choosing to give us a try. \n' +
-          'You can now invite your people to your team. \n\n' +
-          'Once again thanks for choosing us. \n\n' +
-          'Regards \n' },
-          { insert: 'Team Dynki', attributes: { bold: true } }
-        ]},
-        sent: true,
-        created: moment().year(2018).month(10).date(26).hour(21).minute(15).toDate(),
-        author: 'Dynki Team',
-        status: 'Unread',
-        read: false,
-        reading: false,
-        selected: false
-      }]
-      // {
-      //   id: '232425t345eqweq34',
-      //   from: 'Mark Webber',
-      //   to: ['Dean Selvey'],
-      //   subject: 'Abu Dhabi GP',
-      //   body: {
-      //     ops: [
-      //     { insert: 'Hiya, \n\n' +
-      //     'Are you watching the qualifying?.\n' +
-      //     'I hope Danny Ric does well.' },
-      //   ]},
-      //   sent: true,
-      //   created: moment().year(2018).month(10).date(20).hour(9).minute(15).toDate(),
-      //   author: 'Mark Webber',
-      //   status: 'read',
-      //   read: false,
-      //   reading: false,
-      //   selected: false
-      // },
-      // {
-      //   id: '232425t3sdarew45eqweq34',
-      //   from: 'Fernando Alonso',
-      //   to: ['Dean Selvey'],
-      //   subject: 'Abu Dhabi GP',
-      //   body: {
-      //     ops: [
-      //     { insert: 'Hola, \n\n' +
-      //     'Thanks for supporting me on my last race.\n' +
-      //     'Lets hope the bloody car makes it' },
-      //   ]},
-      //   sent: true,
-      //   created: moment().year(2018).month(8).date(19).hour(18).minute(45).toDate(),
-      //   author: 'Mark Webber',
-      //   status: 'read',
-      //   read: false,
-      //   reading: false,
-      //   selected: false
-      // }]
-  }
 
   @Select(BaseState.domainId)
   private domainId$: Observable<string>;
@@ -97,12 +34,51 @@ export class MessagingService {
 
   getMessages() {
     console.log('Messaging::Service::getMessages');
-    this.store.dispatch(new messageActions.RefreshMessages(this.messages));
+    return this.db.collection('domains')
+    .doc(this.domainId)
+    .collection('users')
+    .doc(this.afAuth.auth.currentUser.uid)
+    .collection('messages').snapshotChanges()
+    .pipe(
+      map(a => {
+        const messages = a.reduce((acc, curr) => {
+
+          const newMsg = { id: curr.payload.doc.id, ...curr.payload.doc.data() as any};
+          console.log(newMsg);
+          newMsg.created = moment(newMsg.created.toDate()).toDate();
+          return { messages: [...acc.messages, newMsg]};
+
+        }, { messages: [] });
+
+        return messages;
+      })
+    ).subscribe((messages: IMessages) => {
+      this.store.dispatch(new messageActions.RefreshMessages(messages));
+    })
+
   }
 
-  getMessage(id: string) {
-    console.log('Messaging::Service::getMessage::id::', id);
-    this.store.dispatch(new messageActions.SetCurrentMessage(this.messages.messages.find(m => m.id === id)));
+  getMessage(msgId: string) {
+    console.log('Messaging::Service::getMessage::id::', msgId);
+    return this.db.collection('domains')
+    .doc(this.domainId)
+    .collection('users')
+    .doc(this.afAuth.auth.currentUser.uid)
+    .collection('messages')
+    .doc(msgId).snapshotChanges()
+    .pipe(
+      take(1)
+    )
+    .subscribe(b => {
+        if (b.payload.exists) {
+          const data = { id: b.payload.id, ...b.payload.data() } as any;
+          data.created = moment(data.created.toDate()).toDate();
+
+          this.store.dispatch(new messageActions.SetCurrentMessage(data));
+        } else {
+          return undefined;
+        }
+      });
   }
 
   sortMessages(msgs: IMessage[], order: string) {
